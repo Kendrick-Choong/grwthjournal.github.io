9cea6538f18fd192f4c224a1f7a055f0040986c3
Torin Johnson <tajohnson@mymail.mines.edu>
2020-09-17 13:40:28 -0600

    Updated the signup sheet
    No code was removed, only commented out. Feel free to change back if it doesn't work. Make sure the css file path is correct to display correclty

--- Test/signup.php
+++ Test/signup.php
@@ -101,7 +101,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 }
 ?>
 
-<!DOCTYPE html>
+<!--<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -115,6 +115,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
     <div class="wrapper">
         <h2>Sign Up</h2>
+
+
         <p>Please fill this form to create an account.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
@@ -140,4 +142,242 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         </form>
     </div>
 </body>
+</html> -->
+
+
+
+
+
+<!DOCTYPE HTML>
+<!--
+	Industrious by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+<head>
+	<title>Sign Up</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+</head>
+<body class="is-preload" id="loginBody">
+
+	<!-- Header -->
+	<header id="header">
+		<a href="#" class="logo icon fa-tree"><span class="label">Icon</span></a>
+		<nav>
+			<a href="#menu">Menu</a>
+		</nav>
+	</header>
+
+	<!-- Nav -->
+	<nav id="menu">
+		<ul class="links">
+			<li><a href="index.html">Home</a></li>
+			<li><a href="about.html">About</a></li>
+			<li><a href="signup.html">Sign Up</a></li>
+		</ul>
+	</nav>
+
+	<!-- Heading
+	<div id="heading" >
+		<h1>Login</h1>
+	</div> -->
+	<!-- Main -->
+
+
+
+
+
+	<section id="main" class="wrapper" style="background:linear-gradient(135deg, #1190c2 0%, #12b3a0 74%);">
+		<div class="inner" id="center">
+			<div>
+
+				<section>
+					<div class="content signupbox" style="margin: 0 10%; border-radius:20px;">
+
+						<!--<img src="./images/plant.png" class="plant">-->
+						<h1>Create An Account</h1>
+						
+						<!--<form>
+							<p>
+								Email
+								<input type="text" name="" placeholder="Enter Email" style="width: 100%;">
+							</p>
+							<br />
+							<p>
+								Preferred Name
+								<input type="text" name="" placeholder="Enter Preferred Name" style="width: 100%;">
+							</p>
+							<br />
+							<p>
+								Password
+								<input type="password" name="" placeholder="Enter Password" style="width: 100%;">
+							</p>
+							<br />
+							<p>
+								Confirm Password
+								<input type="password" name="" placeholder="Confirm Password" style="width: 100%;">
+							</p>
+							<br />
+							<p>
+								<input type="radio" name="" value="yes" id="yes">
+								<label for="yes"></label> I have read the <a href="#">Privacy Policy</a> and accept it
+							</p>
+							<p />
+							<br />
+
+							<input type="submit" name="" value="Sign Up"> <br>
+
+							<br />
+							<br />
+
+							<a href="#">Already have an account? Login</a><br>
+
+						</form> -->
+
+
+
+
+						<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+							<div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+								<label>Username</label>
+								<input type="text" name="email" class="form-control" value="<?php echo $email; ?>">
+								<span class="help-block"><?php echo $email_err; ?></span>
+							</div>
+							<div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+								<label>Password</label>
+								<input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+								<span class="help-block"><?php echo $password_err; ?></span>
+							</div>
+							<div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+								<label>Confirm Password</label>
+								<input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+								<span class="help-block"><?php echo $confirm_password_err; ?></span>
+							</div>
+							<div>
+								  <input type="submit" class="btn btn-primary" value="Submit">
+								  <input type="reset" class="btn btn-default" value="Reset">
+							</div>
+							<p>Already have an account? <a href="login.html">Login here</a>.</p>
+						</form>
+
+
+
+
+
+					</div>
+				</section>
+
+			</div>
+		</div>
+	</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<!--<section class="wrapper">
+		<div class="inner">
+			<header class="special">
+
+
+				<div class="signupbox">
+					<!--<img src="./images/plant.png" class="plant">
+					<h1>Create An Account</h1>
+					<br />
+					<form>
+						<p>Email</p>
+						<input type="text" name="" placeholder="Enter Email">
+						<br />
+						<p>Username</p>
+						<input type="text" name="" placeholder="Enter Preferred Name">
+						<br />
+						<p>Password</p>
+						<input type="password" name="" placeholder="Enter Password">
+						<br />
+						<p>Confirm Password</p>
+						<input type="password" name="" placeholder="Confirm Password">
+						<br />
+						<p>
+							<input type="radio" name="" value="yes" id="yes">
+							<label for="yes"></label> I have read the <a href="#">Privacy Policy</a> and accept it
+						</p>
+						<p />
+						<br />
+
+						<input type="submit" name="" value="Sign Up"> <br>
+
+						<br />
+
+						<a href="#">Already have an account? Login</a><br>
+					</form>
+
+
+				</div>
+
+
+
+			</header>
+		</div>
+	</section> -->
+	<!-- Footer -->
+	<footer id="footer">
+		<div class="inner">
+			<div class="content">
+				<section>
+					<h3>Accumsan montes viverra</h3>
+					<p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing. Lorem ipsum dolor vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing sed feugiat eu faucibus. Integer ac sed amet praesent. Nunc lacinia ante nunc ac gravida.</p>
+				</section>
+				<section>
+					<h4>Sem turpis amet semper</h4>
+					<ul class="alt">
+						<li><a href="#">Dolor pulvinar sed etiam.</a></li>
+						<li><a href="#">Etiam vel lorem sed amet.</a></li>
+						<li><a href="#">Felis enim feugiat viverra.</a></li>
+						<li><a href="#">Dolor pulvinar magna etiam.</a></li>
+					</ul>
+				</section>
+				<section>
+					<h4>Magna sed ipsum</h4>
+					<ul class="plain">
+						<li><a href="#"><i class="icon fa-twitter">&nbsp;</i>Twitter</a></li>
+						<li><a href="#"><i class="icon fa-facebook">&nbsp;</i>Facebook</a></li>
+						<li><a href="#"><i class="icon fa-instagram">&nbsp;</i>Instagram</a></li>
+						<li><a href="#"><i class="icon fa-github">&nbsp;</i>Github</a></li>
+					</ul>
+				</section>
+			</div>
+			<div class="copyright">
+				&copy; Untitled. Photos <a href="https://unsplash.co">Unsplash</a>, Video <a href="https://coverr.co">Coverr</a>.
+			</div>
+		</div>
+	</footer>
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/browser.min.js"></script>
+	<script src="assets/js/breakpoints.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+
+</body>
 </html>
+
+
+
+
