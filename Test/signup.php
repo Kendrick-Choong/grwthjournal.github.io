ddfacbbdf84986bc6da13f3fb7386b51bd2e4c7c
Torin Johnson <tajohnson@mymail.mines.edu>
2020-09-17 13:45:34 -0600

    Changed where css was called from

--- Test/signup.php
+++ Test/signup.php
@@ -161,7 +161,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 	<meta name="description" content="" />
 	<meta name="keywords" content="" />
-	<link rel="stylesheet" href="assets/css/main.css" />
+	<link rel="stylesheet" href="main.css" />
 </head>
 <body class="is-preload" id="loginBody">
 
