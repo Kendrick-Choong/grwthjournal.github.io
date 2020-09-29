eda5a739387fefbae20539eff77d044ef4468468
Kenny Choong <46076954+Kendrick-Choong@users.noreply.github.com>
2020-09-29 13:52:25 -0600

    Changed the if statement

--- Grwthjournal/php/signup.php
+++ Grwthjournal/php/signup.php
@@ -17,7 +17,7 @@ $db_password = 'INonlyPassword';
 $db_name = 'aismarth_grwth';
 
 $link = mysqli_connect($db_server,$db_username,$db_password,$db_name);
-if (!$link) {
+if ($link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
