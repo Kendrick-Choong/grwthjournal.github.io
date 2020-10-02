e3aa3d22dc6f2470f7b8cd07c4f6f859ad2d4bd8
Kenny Choong <46076954+Kendrick-Choong@users.noreply.github.com>
2020-10-02 17:09:19 -0600

    Checking to see what's wrong with POST method.

--- Grwthjournal/php/login.php
+++ Grwthjournal/php/login.php
@@ -8,10 +8,10 @@
 // Initialize the session
 session_start();
 // Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+/*if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: userhome.php");
     exit;
-}
+}*/
 // Include config file
 require_once "configInsertAdmin.php";
 
