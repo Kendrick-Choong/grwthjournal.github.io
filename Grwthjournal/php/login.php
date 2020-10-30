9693a46c5146d7b8e392a64f8577a7d52812b500
Kenny Choong <46076954+Kendrick-Choong@users.noreply.github.com>
2020-10-30 17:10:06 -0600

    Fixed?

--- Grwthjournal/php/login.php
+++ Grwthjournal/php/login.php
@@ -41,7 +41,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($email_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT userID, email, preferred_namne, password FROM grwth_login WHERE email = ?";
+        $sql = "SELECT userID, email, preferred_name, password FROM grwth_login WHERE email = ?";
 
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
