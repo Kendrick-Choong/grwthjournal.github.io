41c4b46fd2da65abfde61ccd15538b69b5a3eba3
Kenny Choong <46076954+Kendrick-Choong@users.noreply.github.com>
2020-11-01 16:45:23 -0700

    Updated table in promptshow.php

--- Grwthjournal/php/promptshow.php
+++ Grwthjournal/php/promptshow.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Attempt to execute the prepared statement
         if(mysqli_stmt_execute($stmt)){
           //set up table
-          echo "<table><tr><th>Response</th><th>Time</th></tr>";
+          echo "<table class="styled-table"><tr><th>Response</th><th>Time</th></tr>";
           // Store result
           mysqli_stmt_bind_result($stmt, $col1, $col2);
           //output table data
