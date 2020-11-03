7e99297ac2ffa063b0075624deb8caa3f430b5c5
Kenny Choong <46076954+Kendrick-Choong@users.noreply.github.com>
2020-11-03 14:24:12 -0700

    MAybe fixed?

--- Grwthjournal/php/promptshow.php
+++ Grwthjournal/php/promptshow.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Prompt Show</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<link rel="stylesheet" href="./../assets/css/main.css" />
-	</head>
+
 
 
 <?php
@@ -67,5 +58,14 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
   echo "It's not working";
 }
 ?>
-
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Prompt Show</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="./../assets/css/main.css" />
+	</head>
 </html>
