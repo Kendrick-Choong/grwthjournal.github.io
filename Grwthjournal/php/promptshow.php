fbfc3a5d39c722131de264a94ff859c510b5f915
Kenny Choong <46076954+Kendrick-Choong@users.noreply.github.com>
2020-11-03 14:12:05 -0700

    asdfasdf

--- Grwthjournal/php/promptshow.php
+++ Grwthjournal/php/promptshow.php
@@ -50,6 +50,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
           mysqli_stmt_bind_result($stmt, $col1, $col2);
           //output table data
 					echo "<tbody>";
+					echo mysqli_error($link);
 					if(mysqli_stmt_fetch($stmt) == FALSE){
 						echo mysqli_error($link);
 					}
