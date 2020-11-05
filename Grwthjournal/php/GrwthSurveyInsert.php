96a0c2e9a69afcda3b905568cfae66b52aebafcb
Kenny Choong <46076954+Kendrick-Choong@users.noreply.github.com>
2020-11-05 14:47:27 -0700

    Try this header

--- Grwthjournal/php/GrwthSurveyInsert.php
+++ Grwthjournal/php/GrwthSurveyInsert.php
@@ -98,7 +98,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 } else {
   echo '<script>alert("Please change the survey method to POST.")</script>';
 }
-
 header("refresh:2; url = http://grwthjournal.co/php/signup.php");
+exit();
 mysqli_close($con);
 ?>
