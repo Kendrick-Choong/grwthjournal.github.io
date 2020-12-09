047d4e73268f97d91e47a40cea6318046c5713a0
Kenny Choong <46076954+Kendrick-Choong@users.noreply.github.com>
2020-12-09 15:49:06 -0700

    Edited database connectivity permission

--- Grwthjournal/php/configInsertUser.php
+++ Grwthjournal/php/configInsertUser.php
@@ -6,8 +6,8 @@
 */
 
 $db_server = 'localhost:3306';
-$db_username = 'aismarth_admin';
-$db_password = '0xT1kSEJ**U)';
+$db_username = 'aismarth_user';
+$db_password = 'IPef058sTMPM';
 $db_name = 'aismarth_grwth';
 
 $con = mysqli_connect($db_server,$db_username,$db_password);
