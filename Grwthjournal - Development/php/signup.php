5b529818c9a94f6df1173a31751ac08e9f01590a
Torin Johnson <tajohnson@mymail.mines.edu>
2021-03-15 14:58:19 -0600

    Formatting Modifications Added

--- Grwthjournal - Development/php/signup.php
+++ Grwthjournal - Development/php/signup.php
@@ -141,9 +141,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 	<!-- Header -->
 	<header id="header">
-		<a href="./../index.html" class="logo icon fa-tree"><span class="label">Icon</span></a>
 		<nav>
-			<a href="#menu">Menu</a>
+			<a href="#menu" id="menuButt"></a>
 		</nav>
 	</header>
 
@@ -152,63 +151,60 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 		<ul class="links">
       <li><a href="./../index.html">Home</a></li>
       <li><a href="./../about.html">About</a></li>
-      <li><a href="feedback.php">Provide Feedback</a></li>
-      <li><a href="signup.php">Sign Up</a></li>
       <li><a href="login.php">Login</a></li>
-      <li><a href="dashboard.php">User Dashboard</a></li>
-      <li><a href="logout.php">Logout</a></li>
 		</ul>
 	</nav>
 
 
 	<!-- Main -->
-	<section id="main" class="wrapper" style="background:linear-gradient(135deg, #1190c2 0%, #12b3a0 74%);">
-		<div class="inner" id="center">
-			<div>
+	<section class="largeBack">
+		<section id="main" class="wrapper">
+			<div class="inner" id="center">
+				<div>
 
-				<section>
-					<div class="content signupbox" style="margin: 0 10%; border-radius:20px;">
+					<section>
+						<div class="content signupbox" style="margin: 0 10%; border-radius:20px;">
 
-						<!--<img src="./images/plant.png" class="plant">-->
-						<h1>Create An Account</h1>
+							<!--<img src="./images/plant.png" class="plant">-->
+							<h1>Create An Account</h1>
 
-						<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-							<div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
-								<label>Email</label>
-								<input type="text" name="email" class="form-control" value="<?php echo $email; ?>">
-								<span class="help-block"><?php echo $email_err; ?></span>
-							</div>
-              <div class="form-group">
-								<label>Preferred Name</label>
-								<input type="text" name="preferred_name" class="form-control" value="<?php echo $preferred_name; ?>">
-								<span class="help-block"></span>
-							</div>
-							<div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-								<label>Password</label>
-								<input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-								<span class="help-block"><?php echo $password_err; ?></span>
-							</div>
-							<div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-								<label>Confirm Password</label>
-								<input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
-								<span class="help-block"><?php echo $confirm_password_err; ?></span>
-							</div>
-							<br>
-							<div>
-								  <input type="submit" class="btn btn-primary" value="Submit">
-								  <input type="reset" class="btn btn-default" value="Reset">
-							</div>
-							<br>
-							<p>Already have an account? <a href="login.php">Login here</a></p>
-							<p><a href = "./../privacypolicy.html">Privacy Policy</a></p>
-						</form>
+							<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+								<div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+									<label>Email</label>
+									<input type="text" name="email" class="form-control" value="<?php echo $email; ?>">
+									<span class="help-block"><?php echo $email_err; ?></span>
+								</div>
+				  <div class="form-group">
+									<label>Preferred Name</label>
+									<input type="text" name="preferred_name" class="form-control" value="<?php echo $preferred_name; ?>">
+									<span class="help-block"></span>
+								</div>
+								<div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+									<label>Password</label>
+									<input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+									<span class="help-block"><?php echo $password_err; ?></span>
+								</div>
+								<div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+									<label>Confirm Password</label>
+									<input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+									<span class="help-block"><?php echo $confirm_password_err; ?></span>
+								</div>
+								<br>
+								<div>
+									  <input type="submit" class="btn btn-primary" value="Submit">
+									  <input type="reset" class="btn btn-default" value="Reset">
+								</div>
+								<br>
+								<p>Already have an account? <a href="login.php">Login here</a></p>
+								<p><a href = "./../privacypolicy.html">Privacy Policy</a></p>
+							</form>
 
-					</div>
-				</section>
+						</div>
+					</section>
 
+				</div>
 			</div>
-		</div>
-	</section>
+		</section>
 
 
 	<!-- Footer -->
@@ -216,34 +212,36 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 			<div class="inner">
 				<div class="content">
 					<section>
+					<!-- <li><a href="#"><i class="icon fa-github">&nbsp;</i>Github</a></li> -->
+						<h3>Follow Us</h3>
+						<ul class="plain">
+							<li><a href="#"><i class="icon fa-twitter">&nbsp;</i>Twitter</a></li>
+							<li><a href="#"><i class="icon fa-facebook">&nbsp;</i>Facebook</a></li>
+							<li><a href="#"><i class="icon fa-instagram">&nbsp;</i>Instagram</a></li>
+						</ul>
+					</section>
+		
+					<section>
+						<h3>Navigation</h3>
+						<ul class="plain">
+							<li><a href="./../home.html">Home</a></li>
+							<li><a href="./../privacypolicy.html">Privacy Policy</a></li>
+							<li><a href="./../about.html">About Us</a></li>
+						</ul>
+					</section>
+
+					<section>
 						<h3>Your Privacy is Our Concern</h3>
 						<p>At Grwth, no human will <em>ever</em> see or use your individual data, and the machines will only use it (with your permission) to create custom repsonses for your benefit. If opted in, your data will be aggregated with thousands of other peoples to see large scale trends in population segments.</p>
 					</section>
-					<section>
-						<h4>Navigation</h4>
-						<ul class="alt">
-              <li><a href="./../index.html">Home</a></li>
-              <li><a href="./../about.html">About</a></li>
-              <li><a href="signup.php">Logout</a></li>
-							<li><a href="./../privacypolicy.html">Privacy Policy</a></li>
-						</ul>
-					</section>
-					<!-- Icons for social media if we want to hyperlink our accounts -->
-					<!--<section>
-					<li><a href="#"><i class="icon fa-github">&nbsp;</i>Github</a></li>
-					<h4>Follow Our Journey</h4>
-					<ul class="plain">
-						<li><a href="#"><i class="icon fa-twitter">&nbsp;</i>Twitter</a></li>
-						<li><a href="#"><i class="icon fa-facebook">&nbsp;</i>Facebook</a></li>
-						<li><a href="#"><i class="icon fa-instagram">&nbsp;</i>Instagram</a></li>
-					</ul>
-				</section> -->
+
 				</div>
 				<div class="copyright">
 					&copy; grwthLLC
 				</div>
 			</div>
 		</footer>
+	</section>
 
 	<!-- Scripts -->
 	<script src="./../assets/js/jquery.min.js"></script>
